503342bbf1843017fe164eee44b5e0a9a2fb3a0b
dilanrinconhbtn <937@holbertonschool.com>
2019-06-19 18:43:33 +0000

    puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * my_function - is print with put 
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
+}
