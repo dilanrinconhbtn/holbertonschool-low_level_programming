8ee8563159b0b39fc324ee65673f4c8f8efeeeae
dilanrinconhbtn <937@holbertonschool.com>
2019-06-20 21:21:11 +0000

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	char x;
+	int x;
 
-	for (x = '0'; x <= '9'; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		putchar(x);
+		putchar(x + '0');
 		if (x != 9)
 		{
 			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar(' ');
 	putchar('\n');
 	return (0);
 }
