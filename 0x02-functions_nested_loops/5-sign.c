578fc7a69c5b877a308d90f68568c1718dba3516
dilanrinconhbtn <937@holbertonschool.com>
2019-06-25 15:14:07 +0000

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_sign - check the code for Holberton School students.
+ * @n: character to compare
+ * Return: 0, 1, -1.
  */
 
 int print_sign(int n)
 {
-	if ( n > 0 )
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	if ( n == 0 )
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
