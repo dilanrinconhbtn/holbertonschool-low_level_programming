6e5b39e7233ddd4e3b60cd69e7b7cb3664d5ce74
dilanrinconhbtn <937@holbertonschool.com>
2019-06-25 14:43:57 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
- * alphabetx10 - print 10 alphabet
- *
- *
+ * print_alphabet_x10 - print 10 alphabet
+ * @x for 1
+ * @f for 2
  */
 void print_alphabet_x10(void)
 {
 	int x, f;
 
-	for ( f = 0; f <= 9; f++)
+	for (f = 0; f <= 9; f++)
 	{
-        for ( x = 'a'; x <= 'z'; x++)
-        {
-                _putchar(x);
-        }
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
 	_putchar('\n');
 	}
 }
