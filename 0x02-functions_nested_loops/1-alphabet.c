2af09ba5241da3df1c4422bd9c4f980ca7ebb373
dilanrinconhbtn <937@holbertonschool.com>
2019-06-25 14:29:46 +0000

    alphabet fixing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
  * print_alphabet - print 1 alphabet to a to z.
  * @x: for do a lopp
- * 
+ *
  */
 void print_alphabet(void)
 {
 	int x;
 
-	for ( x = 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		_putchar(x);
 	}
