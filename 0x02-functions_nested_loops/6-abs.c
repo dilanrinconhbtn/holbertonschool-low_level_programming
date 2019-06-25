ef2bfbd1c4a96e5acfb7f8a114f0ff33fc5fb4f1
dilanrinconhbtn <937@holbertonschool.com>
2019-06-25 15:29:05 +0000

    absolute file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _abs - check the code for Holberton School students.
+ * @x: character to compare
  * Return: Always 0.
  */
 int _abs(int x)
 {
-	if ( x < 0 )
+	if (x < 0)
 	{
 		x = x * -1;
 		return (x);
