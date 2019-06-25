c47843278632822da3b5bf8fe2f44352a6e18ec8
dilanrinconhbtn <937@holbertonschool.com>
2019-06-25 14:19:34 +0000

    fixing comments and syntaxis

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,16 @@
 
 /**
  * print_last_digit - print last digit
- *
+ * @z: comparation for do the absolute
  * Return: Always 0.
  */
 int print_last_digit(int z)
 {
+	z=z%10;
 	if ( z < 0 )
 	{
 		z = z * -1;
 	}
-	z=z%10;
 	_putchar(z + '0');
 	return (z);
 }
