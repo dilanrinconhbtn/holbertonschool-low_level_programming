19d3ca73ce0e0e51f14a14fd146d79fe8396f8fa
dilanrinconhbtn <937@holbertonschool.com>
2019-06-26 00:30:54 +0000

    fixing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,25 +13,27 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (a = n; a => 98; a--)
+		for (a = n; a >= 98; a--)
 		{
 			printf("%d", a);
+			if (a != 98)
+			{
+				printf(", ");
+			}
 		}
-		if (n != 98)
-		{
-			printf(", ");
-		}
+		printf("\n");
 	}
 	else if (n < 98)
 	{
 		for (a = n; a <= 98; a++)
 		{
-			printf("%d, ", a);
+			printf("%d", a);
+			if (a != 98)
+			{
+				printf(", ");
+			}
 		}
-		if (n != 98)
-		{
-			printf(", ");
-		}
+		printf("\n");
 	}
 	else
 	{
