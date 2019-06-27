10ba7c196c07b49222241f42f119dcaf354c1319
dilanrinconhbtn <937@holbertonschool.com>
2019-06-27 14:33:10 +0000

    isupper function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _isupper - check the code for Holberton School students.
+ * @c: character to compare
+ * Return: 1 if is Upper, otherwise 0.
  */
 int _isupper(int c)
 {
