11f45cbb6b9c801f9b00387bcd71eb5227ce8e5d
dilanrinconhbtn <937@holbertonschool.com>
2019-06-27 17:52:17 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 2 || a != 4)
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
