bb7e25ef7161bf22e8af9f9eea97f056bafef9de
dilanrinconhbtn <937@holbertonschool.com>
2019-06-27 18:53:57 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n: num to compare
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		_putchar('_');
+	}
+}
