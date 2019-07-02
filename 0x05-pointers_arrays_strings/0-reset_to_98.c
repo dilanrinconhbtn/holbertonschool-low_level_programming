3e1437b77576d740b1819c9e7c4bc0df2610b81b
dilanrinconhbtn <937@holbertonschool.com>
2019-07-02 14:36:10 +0000

    0reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: number to compare
+ * change the value to 98.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
