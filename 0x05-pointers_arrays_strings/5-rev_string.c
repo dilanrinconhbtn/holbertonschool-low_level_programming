3c62cf6e6c72b9a41e707d2e574c9df1eb9a2db5
dilanrinconhbtn <937@holbertonschool.com>
2019-07-02 16:55:41 +0000

    rev a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rev_string - check the code for Holberton School students.
+ * @s: string
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int len = 0;
+	char c;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < (len/2); i++)
+	{
+		c = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = c;
+	}
+}
