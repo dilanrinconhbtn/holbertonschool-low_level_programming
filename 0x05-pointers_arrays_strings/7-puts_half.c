a8ac488ef54080d16559e72b724deeb67c4baedc
dilanrinconhbtn <937@holbertonschool.com>
2019-07-02 18:33:42 +0000

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,9 +11,13 @@ void puts_half(char *str)
 	int i, div;
 	int len = 0;
 
-	while (str[len] <= '\0')
+	while (str[len] != '\0')
 	{
 		len++;
+	}
+	if (len % 2 != 0)
+	{
+		len = len - 1;
 	}
 	div = len / 2;
 	for (i = div; i < len; i++)
