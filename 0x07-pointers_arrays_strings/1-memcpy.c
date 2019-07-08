75b480823a490127c5cff560e49bfe83ab92b22e
dilanrinconhbtn <937@holbertonschool.com>
2019-07-08 15:26:23 +0000

    function memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *destine = dest;
+	char *source = src;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*destine = *source;
+		destine++;
+		source++;
+	}
+	return (dest);
+}
