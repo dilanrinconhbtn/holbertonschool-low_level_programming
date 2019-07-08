db25a00a185e67719961c03e1151876eeca44178
dilanrinconhbtn <937@holbertonschool.com>
2019-07-08 23:38:08 +0000

    print chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_chessboard - check the code for Holberton School students.
+ * @a: 8
+ * Return: no return.
  */
 
 void print_chessboard(char (*a)[8])
