0ad160b776f6a79df76686018fcdb9b031f4cf16
dilanrinconhbtn <937@holbertonschool.com>
2019-07-09 01:13:44 +0000

    set string

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * set_string - check the code for Holberton School students.
+ * @s: pointer to pointer to string 1
+ * @to: pointer string 2
  * Return: Always 0.
  */
 void set_string(char **s, char *to)
