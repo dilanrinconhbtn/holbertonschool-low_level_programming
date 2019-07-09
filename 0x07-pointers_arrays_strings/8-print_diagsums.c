e0920b288977f6e8476673f8707a62387164ba1d
dilanrinconhbtn <937@holbertonschool.com>
2019-07-09 02:36:41 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * print_diagsums - check the code for Holberton School students.
- * @a: data
+ * print_diagsums - print sum of diag of the matrix.
+ * @a: array of data
  * @size: size of array
  * Return: No.
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i, sumright = 0, sumleft = 0;
+	int i, j, sumright = 0, sumleft = 0;
 
-	for (i = 0; i < (size * size); i = i + (size + 1))
+	for (i = 0; i <= (size * size); i = i + (size + 1))
 	{
 		sumleft = sumleft + a[i];
 	}
-	for (i = 0; i < (size * size) - size; j = j + (size - 1))
+	for (j = (size - 1); j < (size * size) - 1; j = j + (size - 1))
 	{
 		sumright = sumright + a[j];
 	}
