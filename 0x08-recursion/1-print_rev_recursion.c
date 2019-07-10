526263558525533fad4231544309a6b838fd6b90
dilanrinconhbtn <937@holbertonschool.com>
2019-07-10 18:47:30 +0000

    print string in rev with recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s: string
  * Return: Always 0.
  */
 
