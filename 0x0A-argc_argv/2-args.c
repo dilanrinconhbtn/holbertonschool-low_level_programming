0dad3069ac89dfb2790b0893ac7a083ce7c53dcb
dilanrinconhbtn <937@holbertonschool.com>
2019-07-12 18:33:07 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print my name
+ * @argc: size
+ * @argv: characters
+ * Return: always 0.
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
