13f683b9a94e7d6d3e23012986e8af31e5ada72e
dilanrinconhbtn <937@holbertonschool.com>
2019-07-12 18:24:04 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print size
+ * @argc: size
+ * 
+ * Return: always 0.
+ */
+
+int main(int argc, char* argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
