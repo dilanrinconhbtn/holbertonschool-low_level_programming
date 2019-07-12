292c7d57666e58360a68c2bcc6ce8360050bc880
dilanrinconhbtn <937@holbertonschool.com>
2019-07-12 20:12:41 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print my name
+ * @argc: size
+ * @argv: characters
+ * Return: always 0.
+ */
+
+int main(int argc, char* argv[])
+{
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add = add + atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
