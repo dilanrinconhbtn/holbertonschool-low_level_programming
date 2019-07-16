fd7f2f00608b0782a18a7ee5d2b81dd8d9ea66c7
dilanrinconhbtn <937@holbertonschool.com>
2019-07-16 17:48:12 +0000

    fixiing outpouts

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,7 +10,7 @@
 
 char *_strdup(char *str)
 {
-	int i, j, size = 0;
+	int i = 0, j;
 	char *new;
 
 	if (str == NULL)
@@ -20,7 +20,6 @@ char *_strdup(char *str)
 	while (str[i] != '\0')
 	{
 		i++;
-		size++;
 	}
 	new = (char *)malloc(i * sizeof(char));
 	if (new == NULL)
@@ -29,7 +28,7 @@ char *_strdup(char *str)
 	}
 	else
 	{
-		for (j = 0; j < (size + 1); j++)
+		for (j = 0; j < (i + 1); j++)
 		{
 			new[j] = str[j];
 		}
