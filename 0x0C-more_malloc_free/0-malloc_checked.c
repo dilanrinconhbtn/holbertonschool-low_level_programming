cb3ef6feac8967498b8e06d8fdad4e8a43ae1adc
dilanrinconhbtn <937@holbertonschool.com>
2019-07-18 16:15:44 +0000

    allocate memory using

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - check the code for Holberton School students.
+ * @b: s
+ * Return: nothing.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *b2;
+
+	b2 = malloc(b);
+	if (b2 == NULL)
+	{
+		free(b2);
+		exit(98);
+	}
+	else
+	{
+		return(b2);
+	}
+}
