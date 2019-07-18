2fbe432a3bda29b1df10e12dc8f3c897b6d9e394
dilanrinconhbtn <937@holbertonschool.com>
2019-07-18 20:40:09 +0000

    array into a two values

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *array_range - prints buffer in hexa
+ * @min: num min
+ * @max: num max
+ *
+ * Return: arr.
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr, i, size = 0;
+
+	size = max - min;
+	arr = malloc(size * sizeof(int) + 1);
+	if (arr == NULL)
+	{
+		free(arr);
+		return ('\0');
+	}
+	for (i = min; i <= size; i++)
+	{
+		arr[i] = i;
+	}
+	return (arr);
+}
