8579d50b5fa5667fa21d7eb8c0d9a90b7122b8f5
dilanrinconhbtn <937@holbertonschool.com>
2019-07-22 17:18:45 +0000

    correction of betty style

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-#define NAME printf (__FILE__"\n")
+#define NAME printf(__FILE__"\n")
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	NAME;
