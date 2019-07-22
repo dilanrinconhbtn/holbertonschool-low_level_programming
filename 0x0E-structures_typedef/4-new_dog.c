9fdcc4437f3613f11284a48b59b3b4d2a1f49bf2
dilanrinconhbtn <937@holbertonschool.com>
2019-07-22 22:46:55 +0000

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Return: dogt
+ * Description: Longer description
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogt;
+
+	dogt = malloc(sizeof(dog_t));
+	if (dogt == NULL)
+		return (NULL);
+
+	dogt->name = name;
+	dogt->age = age;
+	dogt->owner = owner;
+	return (dogt);
+}
