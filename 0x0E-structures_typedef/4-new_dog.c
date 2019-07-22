7c28a4da28751cfee0a914fdc6a251d0877392c4
dilanrinconhbtn <937@holbertonschool.com>
2019-07-22 23:40:12 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,12 +11,32 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0;
 	dog_t *dogt;
+
 	dogt = malloc(sizeof(dog_t));
 	if (dogt == NULL)
 		return (NULL);
+
+	while (*(name + i) != '\0')
+	{
+		i++;
+	}
+	dogt->name = malloc ((i + 1) * sizeof(char));
+	if (name == NULL)
+		return (NULL);
+
 	dogt->name = name;
 	dogt->age = age;
+	i = 0;
+	while (*(owner + i) != '\0')
+	{
+		i++;
+	}
+	dogt->owner = malloc ((i + 1) * sizeof(char));
+	if (owner == NULL)
+		return (NULL);
+
 	dogt->owner = owner;
 	return (dogt);
 }
