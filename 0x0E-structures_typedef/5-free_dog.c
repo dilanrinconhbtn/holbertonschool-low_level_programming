a1da1fad263df0fabe5f00b0ec32c0dfa4f35913
dilanrinconhbtn <937@holbertonschool.com>
2019-07-23 20:29:10 +0000

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
 /**
  * free_dog - Short description
  * @d: First member
  * Description: Longer description
  */
+
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 	{
 		return;
 	}
-	else
-	{
-		free(owner);
-		free(name);
-		free(d);
-	}
+	free(d->owner);
+	free(d->name);
+	free(d);
 }
