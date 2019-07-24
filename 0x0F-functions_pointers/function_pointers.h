dbe621f6245791d98a8105812271ad9f9fd83b25
dilanrinconhbtn <937@holbertonschool.com>
2019-07-24 16:27:44 +0000

    array iterator

--- 0x0F-functions_pointers/function_pointers.h
+++ 0x0F-functions_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef function_pointers
 #define function_pointers
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 #endif /* function_pointers */
