30fdc9b7018c512055817787a61d0787f812fe70
dilanrinconhbtn <937@holbertonschool.com>
2019-07-25 00:26:02 +0000

    atoi

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Struct op
+ *
+ * @argc: amount of arguments
+ * @argv: value of argument
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return (0);
+}
