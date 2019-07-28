1f9696d827be5f5d9dfd5c04ec4d57c1095790e6
dilanrinconhbtn <937@holbertonschool.com>
2019-07-28 15:16:44 +0000

    Fix 1 change second if i != to i < and delete the first if and add to the if i < n -1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,26 +12,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *s;
 
-	if (separator != NULL)
+	va_start (str, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start (str, n);
-		for (i = 0; i < n; i++)
+		s = va_arg(str, char *);
+		if (s == NULL)
 		{
-			s = va_arg(str, char *);
-			if (s == NULL)
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+			if (i < (n - 1) && (separator))
 			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s", s);
-				if (i != (n - 1))
-				{
-					printf("%s", separator);
-				}
+				printf("%s", separator);
 			}
 		}
-		printf("\n");
-		va_end (str);
 	}
+	printf("\n");
+	va_end (str);
 }
