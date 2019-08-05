5e5471c3dbcecd15f50604563f64300e1d1053e2
dilanrinconhbtn <937@holbertonschool.com>
2019-08-05 19:04:58 +0000

    [feature] add file task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free (temp);
+	}
+}
