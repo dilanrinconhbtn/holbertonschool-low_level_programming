eb1866cb8f7cd390077562e0e6ceb48bff4d6006
dilanrinconhbtn <937@holbertonschool.com>
2019-08-06 23:05:09 +0000

    adding more conditions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node, *temp;
 	unsigned int counter = 0;
 
+	if (head == NULL && idx != 0)
+		return (NULL);
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 	{
@@ -38,6 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			node->next = *head;
 			*head = node;
+			return (node);
 		}
 		counter++;
 		temp = temp->next;
