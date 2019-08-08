755fba7fe79e11202fe339e2609db19b01706a61
dilanrinconhbtn <937@holbertonschool.com>
2019-08-08 18:32:55 +0000

    [feature] binari to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - check the code for Holberton School students.
+ * @b: string
+ * Return: Always 0.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, temp = 1;
+	int i, counter = 0;
+
+	while (b[counter] != '\0')
+	{
+		counter++;
+	}
+	for (i = (counter - 1); i >= 0; i--)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			if (b[i] == '1')
+			{
+				num = num + temp;
+			}
+			temp = temp * 2;
+		}
+		else
+		{
+			num = 0;
+			return (num);
+		}
+	}
+	return (num);
+}
