854a534f47e44374c636f161a59f7b5341159e33
dilanrinconhbtn <937@holbertonschool.com>
2019-08-09 00:03:00 +0000

    fixing condition

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,14 +11,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int x = 1;
 
-	if (index > 64)
+	if (index > 32)
 		return (-1);
 	x = x << index;
 	if (*n >= x)
-	{
 		*n = *n - x;
-		return (1);
-	}
-	else
-		return (-1);
+	return (1);
 }
