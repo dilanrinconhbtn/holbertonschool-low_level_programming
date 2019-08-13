8c589dba9096412f80aef1347c6af996e9b4bb38
dilanrinconhbtn <937@holbertonschool.com>
2019-08-13 21:36:46 +0000

    fix - change return 0 for return -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * create_file - check the code for Holberton School students.
+ * @filename: file name
+ * @text_content: text for write in file
  * Return: Always 0.
  */
 
@@ -14,14 +15,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	openfile = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (openfile == -1)
-		return (0);
+		return (-1);
 	if (text_content == NULL)
 		text_content = "";
 	while (text_content[counter])
 	{
 		counter++;
 	}
-	write (openfile, text_content, counter);
-	close (openfile);
+	write(openfile, text_content, counter);
+	close(openfile);
 	return (1);
 }
