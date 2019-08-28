5f76adbd6b77732a3e44d42f36151f0688a26635
dilanrinconhbtn <937@holbertonschool.com>
2019-08-28 21:07:35 +0000

    [feature] prime factor file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (void)
+{
+	unsigned long i, j, number = 612852475143, prime = 0, flag = 0;
+
+	for (i = 0; i <= 50; i++)
+	{
+		if (number % i ==0)
+		{
+			flag = 1;
+			for (j = 2; j <= x / 2; j++)
+			{
+				if (i % j == 0)
+				{
+					flag = 0;
+				}
+			}
+			if (flag == 1)
+			{
+				if (i > prime)
+				{
+					prime = i;
+				}
+			}
+		}
+	}
+	printf("%lu\n", prime);
+	return (0);
+}
