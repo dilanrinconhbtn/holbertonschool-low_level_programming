f00dc9d49d7a99fb4bf3229e62064bbc0a6aea18
dilanrinconhbtn <937@holbertonschool.com>
2019-10-11 23:41:28 +0000

    correction betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,26 +8,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new = NULL;
+	unsigned long int i = 0;
 
-  hash_table_t *new = NULL;
-  unsigned long int i = 0;
-
-  if (size < 1)
-    return (NULL);
-  new = malloc(sizeof(hash_table_t));
-  if (new == NULL)
-    return (NULL);
-  new->array = malloc(sizeof(hash_node_t *) * size);
-  if (new->array == NULL)
-    {
-      free(new);
-      return (NULL);
-    }
-  while (i < size)
-    {
-      new->array[i] = NULL;
-      i++;
-    }
-  new->size = size;
-  return (new);
+	if (size < 1)
+		return (NULL);
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		new->array[i] = NULL;
+		i++;
+	}
+	new->size = size;
+	return (new);
 }
